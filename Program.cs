428edf9cf1906304cc209e919c9656296a9e2180
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-05-26 09:55:17 +0300

    11

--- Program.cs
+++ Program.cs
@@ -10,6 +10,11 @@ namespace Laba
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Ваши команды ");
+            Console.WriteLine("Переименование изображении в соответствии с датой сьемки.");
+            Console.WriteLine("Добавления на изображение отметку, когда фото было сделано.");
+            Console.WriteLine("Сортировка изображений по папкам по годам. ");
+            Console.ReadLine();
         }
     }
 }
