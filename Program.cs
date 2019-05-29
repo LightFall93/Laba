b7c40ac1437733104dd66bca7747e771afd1a0f3
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-05-30 00:28:13 +0300

    30

--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace Laba
 
                     case "2":
                     {
-                        //Meod2();
+                        JustRename();
                     }
                         break;
 
@@ -55,7 +55,7 @@ namespace Laba
         {
             Console.WriteLine("Введите ваш путь к папке изображений, который надо обработать.");
             //string ImagePathEntered = Console.ReadLine();
-            string ImagePathEntered = "E:\\333\\zz";
+            string ImagePathEntered = "E:\\Images";
 
             string dirName = new DirectoryInfo(ImagePathEntered).Name;
 
@@ -72,50 +72,52 @@ namespace Laba
             //    Console.WriteLine(item.Name + " " + item.FullName +" "+ item.CreationTime);
             //}
 
-            DirectoryInfo dirInfo2 = new DirectoryInfo(ImagePathCreated);
-            var file_mas2 = dirInfo2.GetFiles();
+            //DirectoryInfo dirInfo2 = new DirectoryInfo(ImagePathCreated);
+            //var file_mas2 = dirInfo2.GetFiles();
             
 
             //Image myImage = Image.FromFile(@"E:\Images2\IMG_20140912_183647.JPG");
             //PropertyItem propItem = myImage.GetPropertyItem(306);
 
 
-            
-
-
-           
-
-
 
             //foreach (var item in file_mas2)
-            foreach (FileInfo item in dirInfo.GetFiles("*.jpg*"))
-                {
-                    File.Copy(item.FullName, ImagePathCreated + "\\" + item.Name, true);
-                }
+            //foreach (FileInfo item in dirInfo.GetFiles("*.jpg*"))
+            //    {
+            //        File.Copy(item.FullName, ImagePathCreated + "\\" + item.Name, true);
+            //    }
 
-            foreach (var item in file_mas2)
+            foreach (var item in file_mas)
             {
                 Image myImage = Image.FromFile(item.FullName);
                 PropertyItem propItem = myImage.GetPropertyItem(306);
-
+                
                 string sdate2 = Encoding.UTF8.GetString(propItem.Value).Trim();
                 string secondhalf2 = sdate2.Substring(sdate2.IndexOf(" "), (sdate2.Length - sdate2.IndexOf(" ")));
+                secondhalf2 = secondhalf2.TrimEnd(secondhalf2[secondhalf2.Length - 1]);
                 string firsthalf2 = sdate2.Substring(0, 10);
                 firsthalf2 = firsthalf2.Replace(":", "-");
                 secondhalf2 = secondhalf2.Replace(":", "-");
                 sdate2 = firsthalf2 + secondhalf2;
-                Console.WriteLine("sdate2: " + sdate2);
+                sdate2 = sdate2.Replace(" ", "_");
+                
+                //Console.WriteLine("sdate2+1: " + sdate2);
 
 
-
-                //var sourcePath = item.FullName;
-                var sourcePath = $"E:\\333\\zz\\zz_Переименование изображении в соответствии с датой сьемки\\ddsdf.jpg";
+                var sourcePath = item.FullName;
                 //Console.WriteLine("sourcePath: " + sourcePath);
                 var newName = firsthalf2;
                 //Console.WriteLine("firsthalf2: " + firsthalf2);
                 var directory = Path.GetDirectoryName(sourcePath);
                 var destinationPath = Path.Combine(directory, newName);
-                File.Move(sourcePath, destinationPath);
+
+                Console.WriteLine(item.FullName + $@"{ImagePathCreated}\{sdate2}" + ".jpg");
+
+
+                File.Copy(item.FullName, $@"{ImagePathCreated}\{sdate2}" + ".jpg");
+                //item.MoveTo($@"{ImagePathCreated}\{ sdate2}" + ".jpg");
+                //File.Move(item.FullName, $@"{ImagePathCreated}\{sdate2}" + ".jpg");
+                //File.Move(sourcePath, destinationPath);
 
 
 
@@ -138,6 +140,49 @@ namespace Laba
 
 
 
+        }
+
+        public static void JustRename()
+        {
+
+            string ImagePathEntered = "E:\\Images";
+
+            DirectoryInfo dirInfo = new DirectoryInfo(ImagePathEntered);
+
+            var file_mas = dirInfo.GetFiles();
+            FileInfo filemas2 = new FileInfo(ImagePathEntered);
+
+
+
+
+            string tempName;
+            foreach (var items in file_mas)
+            {
+                
+                tempName = Convert.ToString(items.CreationTime);
+                tempName = tempName.Replace(" ", "_");
+                tempName = tempName.Replace(".", "-");
+                tempName = tempName.Replace(":", "_");
+
+                if (!dirInfo.Exists)
+                {
+                    dirInfo.Create();
+                }
+                items.MoveTo($@"{items.DirectoryName}\{tempName}"+".jpg");
+
+
+                Console.WriteLine(tempName); 
+                Console.WriteLine($@"{items.DirectoryName}\{items.CreationTime}");
+                //Console.WriteLine(items.CreationTime+ items.DirectoryName);
+            }
+
+
+
+            //for (int i= 0;i<file_mas.Length;i++)
+            //{
+            //    file_mas.CopyTo();
+            //    //File.Move(item.FullName, ImagePathEntered+ $"\\new"); //переименовывает дописываю нью
+            //}
         }
     }
 }
