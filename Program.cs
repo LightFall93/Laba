5ac01408c704e9ad8124131cf17a08f5b2f3396e
Андрей LightFall <krivosheevandrey93@gmail.com>
2019-06-01 00:08:28 +0300

    01_06

--- Program.cs
+++ Program.cs
@@ -41,7 +41,8 @@ namespace Laba
 
                     case "3":
                     {
-                        Console.WriteLine("tipo metod3 ");
+                        OneDraw();
+                        //JustDraw();
                     }
                         break;
 
@@ -103,6 +104,8 @@ namespace Laba
                         File.Copy(item.FullName, $@"{ImagePathCreated}\{tempName}" + ".jpg", true);
 
                     }
+
+
                 }
             }
 
@@ -124,17 +127,81 @@ namespace Laba
 
             DirectoryInfo dirInfo2 = new DirectoryInfo(ImagePathCreated);
 
+            var file_mas = dirInfo.GetFiles();
+
             if (!dirInfo2.Exists)
             {
                 dirInfo.CreateSubdirectory($"{dirName}_{com2}");
             }
 
+            try
+            {
+                foreach (var item in file_mas)
+                {
+                    PropertyItem propItem = null;
+                    Image myImage = Image.FromFile(item.FullName);
+
+                    try
+                    {
+                        propItem = myImage.GetPropertyItem(306);
+                    }
+                    catch { }
+                    if (propItem != null)
+                    {
+                        string sdate2 = Encoding.UTF8.GetString(propItem.Value).Trim();
+                        string secondhalf2 = sdate2.Substring(sdate2.IndexOf(" "), (sdate2.Length - sdate2.IndexOf(" ")));
+                        secondhalf2 = secondhalf2.TrimEnd(secondhalf2[secondhalf2.Length - 1]);
+                        string firsthalf2 = sdate2.Substring(0, 10);
+                        firsthalf2 = firsthalf2.Replace(":", "-");
+                        secondhalf2 = secondhalf2.Replace(":", "-");
+                        sdate2 = firsthalf2 + secondhalf2;
+                        sdate2 = sdate2.Replace(" ", "_");
+
+                        Image img = Image.FromFile(item.FullName);
+
+                        using (Graphics g = Graphics.FromImage(img))
+                        {
+                            Font drawFont = new Font("Arial", 16);
+                            SolidBrush drawBrush = new SolidBrush(Color.Black);
+                            g.DrawString($"{sdate2}", new Font("Verdana", (float)40),
+                                new SolidBrush(Color.Blue), img.HorizontalResolution / 2, img.VerticalResolution / 2);
+                            img.Save($@"E:\images\{dirName}_{com2}\{item.Name}", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        }
+                    }
+
+                    else
+                    {
+                        Image img = Image.FromFile(item.FullName);
+                        string tempName = Convert.ToString(item.CreationTime);
+                        tempName = tempName.Replace(" ", "_");
+                        tempName = tempName.Replace(".", "-");
+                        tempName = tempName.Replace(":", "_");
+
+                        using (Graphics g = Graphics.FromImage(img))
+                        {
+                            Font drawFont = new Font("Arial", 16);
+                            SolidBrush drawBrush = new SolidBrush(Color.Black);
+                            g.DrawString($"{item.CreationTime}", new Font("Verdana", (float)30),
+                                new SolidBrush(Color.Blue), img.HorizontalResolution / 2, img.VerticalResolution / 2);
+                            img.Save($@"E:\images\{dirName}_{com2}\{item.Name+".jpg"}", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                        }
+
+                    }
+
+                    
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+
+            }
+
         }
-
-
-
-
-
 
         public static void JustRename()
         {
@@ -165,6 +232,139 @@ namespace Laba
                 Console.WriteLine($@"{items.DirectoryName}\{items.CreationTime}");
             }
 
+        }
+
+        public static void JustDraw()
+        {
+            string ImagePathEntered = "E:\\Images";
+
+            
+            DirectoryInfo dirInfo = new DirectoryInfo(ImagePathEntered);
+            var file_mas = dirInfo.GetFiles();
+            FileStream fstream = null;
+
+            foreach (var item in file_mas)
+            {
+                try
+                {
+                    fstream = new FileStream(item.FullName, FileMode.Open);
+
+
+                    Bitmap a = new Bitmap(item.FullName);
+
+                    using (Graphics g = Graphics.FromImage(a))
+                    {
+                        Image img = Image.FromFile(item.FullName); //путь к картинке
+                        
+                        g.DrawString("MUDA", new Font("Verdana", (float)40), //текст на картинке, шрифт и его размер
+                            new SolidBrush(Color.White), 15, img.Height / 2); //месторасположения текста
+
+                        img.Save($@"E:\images\pics\{item.Name}", System.Drawing.Imaging.ImageFormat.Jpeg); //путь и имя сохранения файла
+
+                        
+                    }
+                   
+                }
+                catch (Exception)
+                {
+
+                }
+                finally
+                {
+                    if (fstream != null)
+                        fstream.Close();
+                }
+
+            }
+                
+            
+
+        }
+
+        public static void OneDraw()
+        {
+            //Image img = Bitmap.FromFile(@"E:\images\aaz.jpg"); 
+
+            //Graphics g = Graphics.FromImage(img);
+            //g.DrawString("MUDA", new Font("Verdana", (float)30), //текст на картинке, шрифт и его размер
+            //    new SolidBrush(Color.Red), 15, img.Height / 2); //месторасположения текста
+            //img.Save(@"E:\images\aaz.new.jpg", System.Drawing.Imaging.ImageFormat.Jpeg); 
+            //g = null; 
+            //img = null;
+            ////////////////////////////////////////////////////////////////////
+            //Image img = Bitmap.FromFile(@"E:\images\aaz.jpg");
+
+
+            //using (Graphics g = Graphics.FromImage(img))
+            //    {
+            //        Font drawFont = new Font("Arial", 16);
+            //        SolidBrush drawBrush = new SolidBrush(Color.Black);
+            //        g.DrawString("MUDA", new Font("Verdana", (float)30),
+            //        new SolidBrush(Color.Blue), 15, img.Height / 2);
+            //        img.Save(@"E:\images\aaz.new.jpg", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //    }
+            /////////////////////////////////////////////////////////////////////////////////////////
+            //string ImagePathEntered = "E:\\Images";
+
+            //DirectoryInfo dirInfo = new DirectoryInfo(ImagePathEntered);
+
+            //var file_mas = dirInfo.GetFiles();
+
+            //foreach (var item in file_mas)
+            //{
+            //    Image img = Image.FromFile(item.FullName);
+
+            //    using (Graphics g = Graphics.FromImage(img))
+            //    {
+            //        Font drawFont = new Font("Arial", 16);
+            //        SolidBrush drawBrush = new SolidBrush(Color.Black);
+            //        g.DrawString("MUDA", new Font("Verdana", (float)30),
+            //        new SolidBrush(Color.Blue), 15, img.Height / 2);
+            //        img.Save($@"E:\images\pics\{item.Name}", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            //    }
+
+            //}
+            //////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+            try
+            {
+                string ImagePathEntered = "E:\\Images";
+
+                DirectoryInfo dirInfo = new DirectoryInfo(ImagePathEntered);
+
+                var file_mas = dirInfo.GetFiles();
+
+                
+
+                foreach (var item in file_mas)
+                {
+                    
+                    Image img = Image.FromFile(item.FullName);
+
+                    using (Graphics g = Graphics.FromImage(img))
+                    {
+                        Font drawFont = new Font("Arial", 16);
+                        SolidBrush drawBrush = new SolidBrush(Color.Black);
+                        g.DrawString("MUDA", new Font("Verdana", (float)30),
+                            new SolidBrush(Color.Blue), img.HorizontalResolution/2, img.VerticalResolution / 2);
+                        img.Save($@"E:\images\pics\{item.Name}", System.Drawing.Imaging.ImageFormat.Jpeg);
+
+                    }
+
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               
+            }
+            
         }
     }
 }
